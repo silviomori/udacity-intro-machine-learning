13933638528a53053d9b6ad37f68faf1c2263c67
Silvio Mori Neto <silvio@technomori.com>
2020-03-25 14:30:30 -0300

    feat: Train an SVM Classifier, kernel = linear
    training time: 131.738 s
    predicting time: 15.141 s
    Accuracy: 0.9840728100113766

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,11 +20,17 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 
+svc = SVC(kernel='linear')
+t0 = time()
+svc.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
 
-#########################################################
-### your code goes here ###
+t0 = time()
+predictions = svc.predict(features_test)
+print("predicting time:", round(time()-t0, 3), "s")
 
-#########################################################
-
-
+accuracy = accuracy_score(labels_test, predictions)
+print('Accuracy:', accuracy)
