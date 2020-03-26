d3f7a78e92741cd2f9903479527737fd0e57c9cd
Silvio Mori Neto <silvio@technomori.com>
2020-03-26 17:33:15 -0300

    feat: Train a decision tree to classify emails
    Number of features in the dataset: 3785 (10%)
    
    training time: 19.946 s
    predicting time: 0.013 s
    Accuracy: 0.9755403868031854

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,13 +19,20 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+print('Number of features in the dataset:', len(features_train[0]))
 
 
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 
-#########################################################
-### your code goes here ###
+clf = DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
 
+t0 = time()
+predictions = clf.predict(features_test)
+print("predicting time:", round(time()-t0, 3), "s")
 
-#########################################################
-
-
+accuracy = accuracy_score(labels_test, predictions)
+print('Accuracy:', accuracy)
